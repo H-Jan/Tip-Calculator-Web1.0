c3e34ac274a2c3a7831231983a773476d2f97599
Hani Jandali <hanijandali@hanis-mbp.lan>
2020-09-30 13:22:46 -0700

    Working calculation for both Tip and Total for bill

--- Resources/script.js
+++ Resources/script.js
@@ -6,25 +6,23 @@ const tip_btn = document.querySelector('#tip-btn');
 const total_btn = document.querySelector('#total-btn')
 
 tip_btn.addEventListener('click', calc_tip);
-total_btn.addEventListener('click', calc_total);
 
 function calc_tip () {
- let bill = Number(bill_input.value);
- let percentage = Number(percentage_input.value) / 100;
- let people = Number(people_input.value);
+ let bill = parseInt(bill_input.value);
+ let percentage = parseInt(percentage_input.value) / 100;
+ let people = parseInt(people_input.value);
  let tip = (bill * percentage) / people;
+ let billtotal = bill + (bill * percentage)
  
  let tip_calc = document.querySelector('.tip-section .tip');
  tip_calc.innerText = '$' + tip.toFixed(2) + ((people > 1) ? ' per person' : '');
- // The above loop format was learned onlinex as an append should the value of people be greater than 1
+
+ let total = document.getElementById('totality');
+ total.innerHTML = '$' + billtotal.toFixed(2)
+
+ // if innerText, returns null error. innerHTML with .toFixed converts to string allowing decimal points. 
+// Use of one button to calculate TIP. mix of getElementbyId and innerHTML provided result
+
 }
 
-// The below line of code is proving troublesome in connecting the Total Button to its desired function.
-// The main issue is the loss of function fo rdefined variables, being left undefined or unused. 
-function calc_total () {
- bill_input + calc_tip;
-
- let total = document.querySelector('.total-selection .totality');
- total.innerText = '$' 
-}
 
